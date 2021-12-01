acd79c8a445054bd988cfa529bda673c7ba20e18
xff_ys <ouhuangff@163.com>
2021-12-01 14:12:41 +0800

    :alembic: 算法-二分查找

--- packages/algorithm/查找/二分.ts
+++ packages/algorithm/查找/二分.ts
@@ -1,8 +1,9 @@
 const createArr = (n: number) => [...Array(n).keys()]
 
+// 循环版
 function binarySearch(arr: number[], target: number) {
   let lo = 0,
-    hi = arr.length
+    hi = arr.length - 1
 
   while (lo <= hi) {
     const m = lo + ((hi - lo) >> 1)
@@ -19,4 +20,19 @@ function binarySearch(arr: number[], target: number) {
   return -1
 }
 
-console.log(binarySearch(createArr(10), 11))
+// 递归版
+function binarySearch2(arr: number[], target: number, lo = 0, hi = arr.length - 1): number {
+  if (lo > hi) {
+    return -1
+  }
+  const m = lo + ((hi - lo) >> 1)
+  const x = arr[m]
+
+  if (x > target) {
+    return binarySearch2(arr, target, lo, m - 1)
+  } else if (x < target) {
+    return binarySearch2(arr, target, m + 1, hi)
+  } else {
+    return m
+  }
+}
