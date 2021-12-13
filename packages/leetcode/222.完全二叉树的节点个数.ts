f8741590354bb977d5be48626e375af835d6eeac
xff_ys <ouhuangff@163.com>
2021-12-13 17:32:46 +0800

    :alembic: leetcode

---
+++ packages/leetcode/222.完全二叉树的节点个数.ts
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=typescript
+ *
+ * [222] 完全二叉树的节点个数
+ *
+ * https://leetcode-cn.com/problems/count-complete-tree-nodes/description/
+ *
+ * algorithms
+ * Medium (78.39%)
+ * Likes:    585
+ * Dislikes: 0
+ * Total Accepted:    130.3K
+ * Total Submissions: 166.3K
+ * Testcase Example:  '[1,2,3,4,5,6]'
+ *
+ * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+ *
+ * 完全二叉树
+ * 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h
+ * 层，则该层包含 1~ 2^h 个节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,4,5,6]
+ * 输出：6
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：0
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数目范围是[0, 5 * 10^4]
+ * 0
+ * 题目数据保证输入的树是 完全二叉树
+ *
+ *
+ *
+ *
+ * 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+  if (!root) {
+    return 0
+  }
+
+  let l = root.left,
+    r = root.right,
+    lh = 0,
+    rh = 0
+
+  while (l) {
+    l = l.left
+    lh++
+  }
+
+  while (r) {
+    r = r.right
+    rh++
+  }
+
+  if (lh === rh) {
+    return Math.pow(2, lh + 1) - 1
+  }
+
+  return countNodes(root.left) + countNodes(root.right) + 1
+}
+// @lc code=end
