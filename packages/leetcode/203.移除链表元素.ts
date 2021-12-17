650f0ca69ee3286371fce1a891b6054e4d45044e
X255 <ouhuangff@163.com>
2021-12-17 08:36:27 +0800

    :alembic: leetcode

---
+++ packages/leetcode/203.移除链表元素.ts
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=typescript
+ *
+ * [203] 移除链表元素
+ *
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (52.12%)
+ * Likes:    760
+ * Dislikes: 0
+ * Total Accepted:    264.9K
+ * Total Submissions: 507.8K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,6,3,4,5,6], val = 6
+ * 输出：[1,2,3,4,5]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [], val = 1
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：head = [7,7,7,7], val = 7
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 列表中的节点数目在范围 [0, 10^4] 内
+ * 1
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+  const vnode = new ListNode(-1, head)
+  let temp: ListNode = vnode
+  while (temp.next) {
+    if (temp.next.val === val) {
+      temp.next = temp.next?.next
+    } else {
+      temp = temp.next
+    }
+  }
+
+  return vnode.next
+}
+// @lc code=end
