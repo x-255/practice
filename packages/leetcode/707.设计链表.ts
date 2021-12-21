ffdfccd068420b5fdfa9799211b5bcb5d0c9c191
X255 <ouhuangff@163.com>
2021-12-21 08:10:49 +0800

    :alembic: leetcode

--- packages/leetcode/707.设计链表.ts
+++ packages/leetcode/707.设计链表.ts
@@ -15,22 +15,22 @@
  *
  * 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。val 是当前节点的值，next
  * 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。假设链表中的所有节点都是 0-index 的。
- * 
+ *
  * 在链表类中实现这些功能：
- * 
- * 
+ *
+ *
  * get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
  * addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
  * addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
  * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index
  * 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
  * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 示例：
- * 
+ *
  * MyLinkedList linkedList = new MyLinkedList();
  * linkedList.addAtHead(1);
  * linkedList.addAtTail(3);
@@ -38,45 +38,78 @@
  * linkedList.get(1);            //返回2
  * linkedList.deleteAtIndex(1);  //现在链表是1-> 3
  * linkedList.get(1);            //返回3
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 所有val值都在 [1, 1000] 之内。
  * 操作次数将在  [1, 1000] 之内。
  * 请不要使用内置的 LinkedList 库。
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
+class LinkNode {
+  constructor(
+    public value: number,
+    public next: LinkNode | null = null,
+    public prev: LinkNode | null = null,
+  ) {}
+}
+
 class MyLinkedList {
-    constructor() {
+  private root = new LinkNode(-1)
+  private size = 0
+  private tail: LinkNode | null = null
 
+  private getNode(index: number): LinkNode | null {
+    let tmp: LinkNode | null = this.root
+    while (index--) {
+      tmp = tmp?.next
+      if (!tmp) {
+        return null
+      }
+    }
+    return tmp
+  }
+
+  get(index: number): number {
+    if (index < 0 || index > this.size) {
+      return -1
     }
 
-    get(index: number): number {
+    return this.getNode(index)?.value ?? -1
+  }
 
+  addAtHead(val: number): void {
+    new LinkNode(val, this.root.next?.next, this.root)
+  }
+
+  addAtTail(val: number): void {
+    let tmp = this.root
+
+    while (tmp.next) {
+      tmp = tmp.next
     }
 
-    addAtHead(val: number): void {
+    this.tail = new LinkNode(val, null, tmp)
+  }
 
+  addAtIndex(index: number, val: number): void {
+    const at = this.getNode(index)
+    new LinkNode(val, at?.next?.next, at)
+  }
+
+  deleteAtIndex(index: number): void {
+    const at = this.getNode(index)
+    if (at) {
+      at.prev!.next = at.next
     }
-
-    addAtTail(val: number): void {
-
-    }
-
-    addAtIndex(index: number, val: number): void {
-
-    }
-
-    deleteAtIndex(index: number): void {
-
-    }
+  }
 }
 
 /**
@@ -89,4 +122,3 @@ class MyLinkedList {
  * obj.deleteAtIndex(index)
  */
 // @lc code=end
-
