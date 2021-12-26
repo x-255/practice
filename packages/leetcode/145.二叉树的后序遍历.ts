41551966cbf85cc7c8af91e27b10bae0385c8544
X255 <ouhuangff@163.com>
2021-12-26 23:05:29 +0800

    :alembic: leetcode

---
+++ packages/leetcode/145.二叉树的后序遍历.ts
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=typescript
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.22%)
+ * Likes:    730
+ * Dislikes: 0
+ * Total Accepted:    341.5K
+ * Total Submissions: 454K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的 后序 遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3
+ *
+ * 输出: [3,2,1]
+ *
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null, res: number[] = []): number[] {
+  if (!root) return []
+
+  postorderTraversal(root.left, res)
+  postorderTraversal(root.right, res)
+  res.push(root.val)
+
+  return res
+}
+// @lc code=end
