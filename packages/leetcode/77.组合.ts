bd51d1b77683d204a33587874c6ebbf5723a1846
X255 <ouhuangff@163.com>
2022-01-06 23:27:12 +0800

    :alembic: leetcode

--- packages/leetcode/77.组合.ts
+++ packages/leetcode/77.组合.ts
@@ -14,14 +14,14 @@
  * Testcase Example:  '4\n2'
  *
  * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
- * 
+ *
  * 你可以按 任何顺序 返回答案。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：n = 4, k = 2
  * 输出：
  * [
@@ -32,27 +32,44 @@
  * ⁠ [1,3],
  * ⁠ [1,4],
  * ]
- * 
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：n = 1, k = 1
  * 输出：[[1]]
- * 
- * 
- * 
+ *
+ *
+ *
  * 提示：
- * 
- * 
- * 1 
- * 1 
- * 
- * 
+ *
+ *
+ * 1
+ * 1
+ *
+ *
  */
 
 // @lc code=start
 function combine(n: number, k: number): number[][] {
+  const res: number[][] = []
 
-};
+  const backtracking = (lo: number, path: number[]) => {
+    const len = path.length
+    if (len === k) {
+      res.push([...path])
+      return
+    }
+
+    for (let i = lo; i <= n - (k - len) + 1; i++) {
+      path.push(i)
+      backtracking(i + 1, path)
+      path.pop()
+    }
+  }
+
+  backtracking(1, [])
+
+  return res
+}
 // @lc code=end
-
