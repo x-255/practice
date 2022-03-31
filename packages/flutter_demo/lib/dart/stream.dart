da34379ff0b7d3ed452f9af4fdd1b7b38726e1f4
xff_ys <ouhuangff@163.com>
2022-03-31 13:09:32 +0800

    flutter_demo

--- packages/flutter_demo/lib/dart/stream.dart
+++ packages/flutter_demo/lib/dart/stream.dart
@@ -8,8 +8,9 @@ void printStream(Stream s) async {
   }
 }
 
-final fu1 = Future.delayed(Duration(seconds: 1), () => 111);
+final fu1 = Future.delayed(Duration(seconds: 2), () => 111);
 final fu2 = Future.delayed(Duration(seconds: 1), () => 222);
+final fu3 = Future.delayed(Duration(seconds: 1), () => Future.error('errr'));
 
 // 单订阅流
 
@@ -23,7 +24,66 @@ void fromFuture() async {
   printStream(s);
 }
 
+void fromFutures() async {
+  var s = Stream.fromFutures([fu1, fu2]);
+  printStream(s);
+}
+
+void fromIterable() async {
+  var s = Stream.fromIterable([11, 22, 33]);
+  printStream(s);
+}
+
+void value() async {
+  var s = Stream.value(23123);
+  printStream(s);
+}
+
+void forEach() async {
+  var s = Stream.fromFutures([fu1, fu2]);
+  s.forEach(print);
+}
+
+void listen() async {
+  var s = Stream.fromFutures([fu1, fu3, fu2]);
+  s.listen(
+    print,
+    cancelOnError: true,
+    onDone: () => print('done'),
+    onError: (err) => print('err====${err}'),
+  );
+}
+
+void take() async {
+  var s = Stream.periodic(Duration(seconds: 1), id);
+  s = s.take(3);
+  printStream(s);
+}
+
+void takeWhile() async {
+  var s = Stream.periodic(Duration(seconds: 1), id);
+  s = s.takeWhile((x) => x <= 3);
+  printStream(s);
+}
+
+void skip() async {
+  var s = Stream.periodic(Duration(seconds: 1), id);
+  s = s.take(5);
+  s = s.skip(2);
+  printStream(s);
+}
+
+void skipWhile() async {
+  var s = Stream.periodic(Duration(seconds: 1), id);
+  s = s.take(5);
+  s = s.skipWhile((x) {
+    print('x====${x}');
+
+    return x != 2;
+  });
+  printStream(s);
+}
+
 void main() {
-  // periodic();
-  fromFuture();
+  skipWhile();
 }
