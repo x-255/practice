1d528130496fc4e25d3f269c4d5f21188054b2a0
xff_ys <ouhuangff@163.com>
2022-04-28 17:36:24 +0800

    pinia-type

---
+++ packages/z7z8/src/pinia-type.ts
@@ -0,0 +1,67 @@
+type Getter<T> = T extends Record<string, (...args: any[]) => any>
+  ? { readonly [K in keyof T]: ReturnType<T[K]> }
+  : {}
+
+declare function defineStore<Id extends string, S, G, A>(store: {
+  id: Id
+  state: () => S
+  getters: G & ThisType<Readonly<S> & Getter<G>>
+  actions: A & ThisType<S & Getter<G> & A>
+}): S & Getter<G> & A
+
+const store = defineStore({
+  id: '',
+  state: () => ({
+    num: 0,
+    str: '',
+  }),
+  getters: {
+    stringifiedNum() {
+      // @ts-expect-error
+      this.num += 1
+
+      return this.num.toString()
+    },
+    parsedNum() {
+      return parseInt(this.stringifiedNum)
+    },
+  },
+  actions: {
+    init() {
+      this.reset()
+      this.increment()
+    },
+    increment(step = 1) {
+      this.num += step
+    },
+    reset() {
+      this.num = 0
+
+      // @ts-expect-error
+      this.parsedNum = 0
+
+      return true
+    },
+    setNum(value: number) {
+      this.num = value
+    },
+  },
+})
+
+// @ts-expect-error
+store.nopeStateProp
+// @ts-expect-error
+store.nopeGetter
+// @ts-expect-error
+store.stringifiedNum()
+store.init()
+// @ts-expect-error
+store.init(0)
+store.increment()
+store.increment(2)
+// @ts-expect-error
+store.setNum()
+// @ts-expect-error
+store.setNum('3')
+store.setNum(3)
+const r = store.reset()
