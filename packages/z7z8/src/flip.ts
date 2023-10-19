07917f9b59815f65a1a9500e3972a3140874b6fe
finn.xu <ouhuangff@163.com>
2023-10-19 15:44:12 +0800

    flip重构

--- packages/z7z8/src/flip.ts
+++ packages/z7z8/src/flip.ts
@@ -9,14 +9,14 @@ function render() {
 
   app.innerHTML = `<button id="shuffle">shuffle</button>
     <div class="box">
-        ${createItems(10)}
+        ${createItems(12)}
     </div>`
 
   const style = document.createElement('style')
   style.innerHTML = `
     .box {
         display: grid;
-        grid-template-columns: repeat(2, 100px);
+        grid-template-columns: repeat(3, 100px);
         gap: 10px;
         margin-top: 10px;
       }
@@ -40,34 +40,47 @@ function getLocation(el: Element) {
   return el.getBoundingClientRect()
 }
 
-function flip(oldLocation: DOMRect, newLocation: DOMRect, el: Element) {
-  // 将元素放到到初始位置再运用动画移动到结束位置
-  el.animate(
-    [
+function flip(els: Element | Element[], handler: () => void) {
+  if (!Array.isArray(els)) {
+    els = [els]
+  }
+
+  const locatoinsMap = new Map()
+
+  els.forEach((el) => {
+    locatoinsMap.set(el, getLocation(el))
+  })
+
+  handler()
+
+  els.forEach((el) => {
+    const oldLocation = locatoinsMap.get(el)
+    const newLocation = getLocation(el)
+
+    el.animate(
+      [
+        {
+          transform: `translate(${oldLocation.left - newLocation.left}px, ${
+            oldLocation.top - newLocation.top
+          }px)`,
+        },
+        { transform: 'translate(0, 0)' },
+      ],
       {
-        transform: `translate(${oldLocation.left - newLocation.left}px, ${
-          oldLocation.top - newLocation.top
-        }px)`,
-      },
-      { transform: 'translate(0, 0)' },
-    ],
-    {
-      duration: 500,
-      easing: 'ease-in-out',
-    }
-  )
+        duration: 500,
+        easing: 'ease-in-out',
+      }
+    )
+  })
 }
 
 function shuffle() {
   const box = document.querySelector('.box')!
   const items = Array.from(box.children)
 
-  items.sort(() => Math.random() - 0.5)
-  const locatoins = items.map(getLocation) // 记录初始位置
-
-  items.forEach((item) => box.appendChild(item))
-  items.forEach((item, index) => {
-    flip(locatoins[index], getLocation(item), item) // 记录结束位置
+  flip(items, () => {
+    items.sort(() => Math.random() - 0.5)
+    items.forEach((item) => box.appendChild(item))
   })
 }
 
