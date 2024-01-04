8a86c8869d6194855658210172c77777bccc0102
x-255 <ouhuangff@163.com>
2024-01-04 21:09:00 +0800

    rxjs

--- packages/rxjs/src/index.ts
+++ packages/rxjs/src/index.ts
@@ -1,7 +1,8 @@
-import { fromEvent, interval, map, withLatestFrom } from 'rxjs'
+import { fromEvent, timestamp } from 'rxjs'
 
-const clicks = fromEvent(document, 'click')
-const timer1 = interval(1000).pipe(map((x) => `interval1: ${x}`))
-const timer2 = interval(1000).pipe(map((x) => `interval2: ${x}`))
-const result = clicks.pipe(withLatestFrom(timer1, timer2))
-result.subscribe((x) => console.log(x))
+const clickWithTimestamp = fromEvent(document, 'click').pipe(timestamp())
+
+// Emits data of type { value: PointerEvent, timestamp: number }
+clickWithTimestamp.subscribe((data) => {
+  console.log(data)
+})
