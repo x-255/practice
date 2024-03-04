b9a7ae8d5c8fbe60056170d26d5ec39ee924fde7
x-255 <ouhuangff@163.com>
2024-03-04 21:49:32 +0800

    javaweb

---
+++ packages/java_web/maven_p1/src/test/java/com/javaweb/DemoTest.java
@@ -0,0 +1,20 @@
+package com.javaweb;
+
+import org.junit.Test;
+
+public class DemoTest {
+  @Test
+  public void test1() {
+    System.out.println("DemoTest.test()");
+  }
+
+  @Test
+  public void test2() {
+    System.out.println("DemoTest.test()");
+  }
+
+  @Test
+  public void test3() {
+    System.out.println("DemoTest.test()");
+  }
+}
