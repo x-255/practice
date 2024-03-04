4b533f928589941a3727523c36adbcae27d88175
x-255 <ouhuangff@163.com>
2024-03-04 22:59:29 +0800

    spring boot

---
+++ packages/java_web/spring_boot/sboot/src/test/java/com/sboot/SbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
