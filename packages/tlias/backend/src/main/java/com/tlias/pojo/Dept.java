cabbf684107fe2d2279188bfbc587e9e47fec1e7
x-255 <ouhuangff@163.com>
2024-03-17 23:06:04 +0800

    tlias dept

--- packages/tlias/backend/src/main/java/com/tlias/pojo/Dept.java
+++ packages/tlias/backend/src/main/java/com/tlias/pojo/Dept.java
@@ -1,10 +1,12 @@
 package com.tlias.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -12,6 +14,8 @@ import java.time.LocalDate;
 public class Dept {
     private  Integer id;
     private String name;
-    private LocalDate createTime;
-    private LocalDate updateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
 }
