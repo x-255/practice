7e3df0a8d8e7ebce09f93beb9b2e243633cb80c2
x-255 <ouhuangff@163.com>
2024-03-24 17:25:41 +0800

    tlias

--- packages/tlias/backend/src/main/java/com/tlias/utils/AliOSSUtils.java
+++ packages/tlias/backend/src/main/java/com/tlias/utils/AliOSSUtils.java
@@ -1,33 +1,33 @@
 package com.tlias.utils;
 
-import com.aliyun.oss.ClientException;
 import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.OSSException;
-import com.aliyun.oss.model.PutObjectRequest;
 import com.aliyun.oss.model.PutObjectResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
 @Component
 public class AliOSSUtils {
-    private String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
-    private String accessKeyId = "LTAI5tSjz2GnCKYmbGzUjVae";
-    private String accessKeySecret = "gg55RDjemqwNMSpcegECHUdPz81Yy9";
-    private  String bucketName = "ff-web-tlias";
+    @Autowired
+    AliOSSProperties aliOSSProperties;
 
     public String upload(MultipartFile file) throws IOException {
+        String endpoint = aliOSSProperties.getEndpoint();
+        String accessKeyId = aliOSSProperties.getAccessKeyId();
+        String accessKeySecret = aliOSSProperties.getAccessKeySecret();
+        String bucketName = aliOSSProperties.getBucketName();
+
         InputStream inputStream = file.getInputStream();
         String originalFilename = file.getOriginalFilename();
         assert originalFilename != null;
         String fileName = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
 
-        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId,accessKeySecret);
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         PutObjectResult putObjectResult = ossClient.putObject(bucketName, fileName, inputStream);
 
         String[] endpointArr = endpoint.split("//");
