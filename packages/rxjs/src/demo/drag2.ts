2bb9bb4bff1a464a5b1f436d007ad28a383cfcb3
x-255 <ouhuangff@163.com>
2024-04-14 18:52:30 +0800

    rxjs

--- packages/rxjs/src/demo/drag2.ts
+++ packages/rxjs/src/demo/drag2.ts
@@ -1,33 +1,50 @@
-import { concatMap, from, fromEvent, map, mergeMap, takeUntil, timer, zip } from "rxjs"
-import { createDragBox } from "../utils"
+import {
+  animationFrameScheduler,
+  concatMap,
+  from,
+  fromEvent,
+  map,
+  mergeMap,
+  observeOn,
+  takeUntil,
+  timer,
+  zip,
+} from 'rxjs'
+import { createDragBox } from '../utils'
 
 const first = <T, K>(tuple: [T, K]) => tuple[0]
 
-const boxs = Array(10).fill(0).map(createDragBox)
+const boxs = Array(100).fill(0).map(createDragBox)
 const firstBox = boxs[0]
 firstBox.style.zIndex = '9'
 
+const mouseDown$ = fromEvent<MouseEvent>(firstBox, 'mousedown')
+const mouseMove$ = fromEvent<MouseEvent>(document, 'mousemove')
+const mouseUp$ = fromEvent<MouseEvent>(document, 'mouseup')
+const delay$ = zip(from(boxs), timer(0, 50)).pipe(map(first))
 
-const mouseDown$ = fromEvent<MouseEvent>(firstBox, "mousedown")
-const mouseMove$ = fromEvent<MouseEvent>(document, "mousemove")
-const mouseUp$ = fromEvent<MouseEvent>(document, "mouseup")
-const delay$ = zip(from(boxs), timer(0, 100)).pipe(map(first))
-
-mouseDown$.pipe(
-  concatMap(ev => mouseMove$.pipe(
-    map(e => ({
-      x: e.clientX - ev.offsetX,
-      y: e.clientY - ev.offsetY
-    })),
-    takeUntil(mouseUp$)
-  )),
-  mergeMap(pos => delay$.pipe(
-    map(box => ({
-      box,
-      ...pos
-    }))
-  ))
-).subscribe(({box, x, y}) => {
-  box.style.left = `${x}px`
-  box.style.top = `${y}px`
-})
+mouseDown$
+  .pipe(
+    concatMap((ev) =>
+      mouseMove$.pipe(
+        map((e) => ({
+          x: e.clientX - ev.offsetX,
+          y: e.clientY - ev.offsetY,
+        })),
+        takeUntil(mouseUp$)
+      )
+    ),
+    mergeMap((pos) =>
+      delay$.pipe(
+        map((box) => ({
+          box,
+          ...pos,
+        }))
+      )
+    ),
+    observeOn(animationFrameScheduler)
+  )
+  .subscribe(({ box, x, y }) => {
+    box.style.left = `${x}px`
+    box.style.top = `${y}px`
+  })
