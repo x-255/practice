246d0cb5420ef2c3baf65c6462bc547aa2d5739b
x-255 <ouhuangff@163.com>
2024-04-24 22:55:50 +0800

    update gitignore

--- packages/java_web/cloud-demo/cloud-order/src/main/java/com/ff/cloudorder/CloudOrderApplication.java
+++ packages/java_web/cloud-demo/cloud-order/src/main/java/com/ff/cloudorder/CloudOrderApplication.java
@@ -1,5 +1,7 @@
 package com.ff.cloudorder;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -16,5 +18,10 @@ public class CloudOrderApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public IRule randomRule() {
+        return new RandomRule();
     }
 }
