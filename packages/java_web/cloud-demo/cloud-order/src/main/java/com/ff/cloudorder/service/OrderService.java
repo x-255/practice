8f1faf9eaa7d67302473007549b4c5a63b187ea1
x-255 <ouhuangff@163.com>
2024-04-25 20:58:48 +0800

    remove old spring cloud

--- packages/java_web/cloud-demo/cloud-order/src/main/java/com/ff/cloudorder/service/OrderService.java
+++
@@ -1,7 +0,0 @@
-package com.ff.cloudorder.service;
-
-import com.ff.cloudorder.pojo.Order;
-
-public interface OrderService {
-    public Order getOrderById(long id);
-}
--- packages/java_web/cloud-demo/cloud-user/src/main/java/com/ff/clouduser/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.ff.clouduser.service;
-
-import com.ff.clouduser.pojo.User;
-
-public interface UserService {
-    public User getUserById(long id);
-}
