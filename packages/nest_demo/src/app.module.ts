e195dedc831d060e20595d010eb460429383c648
x-255 <ouhuangff@163.com>
2025-01-12 21:54:30 +0800

    nest

--- packages/nest_demo/src/app.module.ts
+++ packages/nest_demo/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'src/configuration';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: '123123',
+      database: 'testdb',
+      synchronize: true,
+      logging: ['error'],
+      entities: [],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- packages/nest_demo/src/user/user.controller.ts
+++ packages/nest_demo/src/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private configService: ConfigService) {}
 
   @Get()
   getUsers() {
+    console.log(this.configService.get('db'))
     return this.userService.getUsers()
   }
 }
