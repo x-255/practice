52f2198c56b66c7ca9b990ac9ddca2d4f7c0a2e6
x-255 <ouhuangff@163.com>
2025-01-21 21:47:39 +0800

    nest global exception filter

--- packages/nest_demo/src/app.module.ts
+++ packages/nest_demo/src/app.module.ts
@@ -8,6 +8,11 @@ import { Role } from './role/role.entity'
 import { Profile } from './user/profile.entity'
 import { User } from './user/user.entity'
 import { UserModule } from './user/user.module'
+import { utilities, WinstonModule } from 'nest-winston'
+import { format, transports } from 'winston'
+import 'winston-daily-rotate-file'
+import { APP_FILTER } from '@nestjs/core'
+import { HttpExceptionFilter } from './filters/http-exception-filter'
 
 @Module({
   imports: [
@@ -15,19 +20,42 @@ import { UserModule } from './user/user.module'
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory (configService: ConfigService) {
+      useFactory(configService: ConfigService) {
         const dbConfig = configService.get('db')
         return {
           ...dbConfig,
           logging: process.env.NODE_ENV === 'development' ? true : ['error'],
           entities: [User, Profile, Log, Role],
         }
-      }
+      },
+    }),
+    WinstonModule.forRoot({
+      transports: [
+        new transports.Console({
+          format: format.combine(
+            format.timestamp(),
+            utilities.format.nestLike()
+          ),
+        }),
+        new transports.DailyRotateFile({
+          dirname: 'logs',
+          filename: 'application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d'
+        })
+      ],
     }),
     UserModule,
     LogModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- packages/nest_demo/src/log/log.controller.ts
+++ packages/nest_demo/src/log/log.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common'
 import { LogService } from './log.service'
 import { Log } from './log.entity'
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+import { Logger } from 'winston'
 
 @Controller('logs')
 export class LogController {
-  constructor(private logService: LogService) {}
+  constructor(
+    private logService: LogService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger
+  ) {}
 
   @Get()
   findAll() {
+    this.logger.info('Finding all logs')
     return this.logService.findAll()
   }
 
